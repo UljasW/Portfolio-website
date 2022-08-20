4cc1cd851dfaf0e2e69b3c54eb3a961b33a3f699
Uljas Wennström <uljas.wennstrom@gmail.com>
2022-08-20 12:31:21 +0200

    -

---
+++ src/LargeWidget.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './Widget.css';
+
+
+function LargeWidget() {
+  return (
+    <div className="LargeSize">
+      <h1 >Hi there, i am Uljas Wennström.</h1>
+      <h2>A 17 year old fullstack .NET and React developer. <br />
+        I create websites, crossplatform native apps and APIs.
+      </h2>
+    </div>
+  );
+}
+
+export default LargeWidget;
