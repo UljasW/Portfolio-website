122bc72fdbdc0342f0e3d50959cb68b38c4d3ce4
Uljas Wennstrom <uljas.wennstrom@gmail.com>
2022-09-07 17:29:10 +0200

    -

--- src/ContactMe.js
+++ src/ContactMe.js
@@ -14,6 +14,7 @@ function ContactMe() {
             <button className="Button" onClick={OpenMail}>
                 My email
             </button>
+            <div className='margin' />
             <form action="https://www.linkedin.com/in/uljas-wennstrom-823b47215/">
                 <input className='Button' type="submit" value="My linkedin" />
             </form>
